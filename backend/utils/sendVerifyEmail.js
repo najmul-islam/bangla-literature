5695af20a46507b51c38ebb825c42876c4f97ab5
najmul islam <najmulislam519@gmail.com>
2024-05-17 15:51:11 +0600

    modify code, fix sending dubble mail

--- backend/utils/sendVerifyEmail.js
+++ backend/utils/sendVerifyEmail.js
@@ -1,24 +1,23 @@
 const nodemailer = require("../configs/nodemailer");
 
 const sendVerifyEmail = async (user) => {
-  const transforter = nodemailer();
-  const mailOption = await transforter.sendMail({
-    from: `"bangla-literature" <${process.env.NODEMAILER_EMAIL}>`,
-    to: user.email,
-    subject: "bangla-literature verification link",
-    html: `
+  try {
+    const transforter = nodemailer();
+    const mailOption = {
+      from: `"bangla-literature" <${process.env.NODEMAILER_EMAIL}>`,
+      to: user.email,
+      subject: "bangla-literature verification link",
+      html: `
     <p>Hello ${user.name}, verify your email by clicking this link</p>
     <a href='${process.env.CLIENT_URL}/verify-email?verifyToken=${user.verifyToken}&name=${user.name}&email=${user.email}'>Verify Your Email</a>
     `,
-  });
+    };
 
-  transforter.sendMail(mailOption, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Verification email sent");
-    }
-  });
+    await transforter.sendMail(mailOption);
+    console.log("Verification email sent");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = sendVerifyEmail;
