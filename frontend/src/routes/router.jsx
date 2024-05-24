22869ecbd99f808016fff0805ce741ddfe8a2268
najmul islam <najmulislam519@gmail.com>
2024-05-24 20:38:27 +0600

    add loading, notfound, contact, faq, privacy, terams page

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -9,15 +9,20 @@ import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import ApiPage from "../pages/public/ApiPage";
 import BlogPage from "../pages/public/BlogPage";
+import ContactUsPage from "../pages/public/ContactUsPage";
 import ExamplesPage from "../pages/public/ExamplesPage";
+import FaqPage from "../pages/public/FaqPage";
 import HomePage from "../pages/public/HomePage";
+import NotFoundPage from "../pages/public/NotFoundPage";
+import PrivacyPolicyPage from "../pages/public/PrivacyPolicyPage";
+import TermsAndConditionsPage from "../pages/public/TermsAndConditionsPage";
+import ChangePasswordPage from "../pages/user/ChangePasswordPage";
+import ProfilePage from "../pages/user/ProfilePage";
 
 // user pages
 import ForgotPasswordPage from "../pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "../pages/auth/ResetPasswordPage";
 import VerifyEmailPage from "../pages/auth/VerifyEmailPage";
-import ChangePasswordPage from "../pages/user/ChangePasswordPage";
-import ProfilePage from "../pages/user/ProfilePage";
 
 const Router = () => {
   return (
@@ -32,6 +37,10 @@ const Router = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
+        <Route path="/privacy" element={<PrivacyPolicyPage />} />
+        <Route path="/terms" element={<TermsAndConditionsPage />} />
+        <Route path="/faq" element={<FaqPage />} />
       </Route>
 
       <Route path="/user/" element={<UserLayout roles={["user"]} />}>
@@ -42,6 +51,7 @@ const Router = () => {
       {/* <Route path="/moderator/" element={<UserLayout roles={["moderator"]} />}>
         <Route path="profile" element={<ProfilePage />} />
       </Route> */}
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
