f00da9c5fe2736be85d7aef17a77da9ad7ff1250
najmul islam <najmulislam519@gmail.com>
2024-05-25 11:32:42 +0600

    modify apikey function

--- backend/middlewares/apikeyMiddleware.js
+++ backend/middlewares/apikeyMiddleware.js
@@ -1,16 +1,21 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const isApikey = asyncHandler(async (req, res, next) => {
-  let apikey;
-  if (req.headers["X-Api-Key"]) {
-    apikey = req.headers["X-Api-Key"];
-    req.apikey = apikey;
-    next();
-  } else {
+  const apikey = req.headers["x-api-key"];
+  if (!apikey) {
     return res
       .status(401)
-      .json({ message: "API key is missing in your header" });
+      .json({ message: "API key is missing in your headersss" });
   }
+
+  const user = await User.findOne({ apikey });
+  if (!user) {
+    return res.status(403).json({ message: "Invalid API key" });
+  }
+
+  req.apikey = apikey;
+  next();
 });
 
 module.exports = { isApikey };
