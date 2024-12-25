6af4f770387d923759f95844fd76f812e8736f88
najmul-islam <najmulislam519@gmail.com>
2024-12-25 12:02:16 +0600

    add user reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
 import authReducer from "../features/auth/authSlice";
+import userReducer from "../features/user/userSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
